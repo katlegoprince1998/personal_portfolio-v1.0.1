09a1379533f978d91291af62f237ea69de93ee2c
katlegoprince1998 <katlegoprince357@gmail.com>
2023-09-04 11:37:51 +0200

    text animation

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -1,12 +1,29 @@
 import React from 'react';
 import MyImage from "../Assets/dev.jpg";
+import {Typewriter, Cursor} from "react-simple-typewriter";
 
 const About = () => {
+
   return (
     <div className='pt-36 px-6 grid grid-cols-1 gap-8 justify-center items-center md:flex' id='Home'>
-        <div>
+        <div className='w-full grid justify-center'>
            <div>
-            <h1 className='text-[#2e79ba] text-4xl  font-extrabold font-myname md:text-7xl'>DevOps Engineer</h1>
+            <h1 className='text-[#2e79ba] text-4xl  font-extrabold font-myname md:text-4xl'>
+            I'm a {' '}
+        <span style={{ color: 'white', fontWeight: 'bold' }}>
+          {/* Style will be inherited from the parent element */}
+          <Typewriter
+            words={['DevOpps Engineer', 'Software Engineer', 'Database Designer']}
+            loop={5}
+            cursor
+            cursorStyle='_'
+            typeSpeed={70}
+            deleteSpeed={50}
+            delaySpeed={1000}
+           
+          />
+        </span>
+              </h1>
            </div>
            <div className=''>
             <img  className='rounded-3xl my-4 md:h-[450px]' src={MyImage} alt="" />
